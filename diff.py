efb577331f5e59ad80fa351aac51ce02f897d149
Lucia Rolon <lucia.rolon@codetria.com>
2025-02-19 12:33:31 -0300

    Added user sector validation

--- diff.py
+++ diff.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import sys
 
 
 def read_binary_file(filepath):
@@ -7,22 +8,54 @@ def read_binary_file(filepath):
         return f.read()
 
 
+def is_valid_sector(address: int) -> bool:
+    """
+    Returns whether or nor an address is within the user data sector
+    :param address: Current Hex address
+    :return: True if it's between the user data sector, false if it's not.
+    """
+    sector = address % 0x930
+    return 0x18 <= sector <= 0x817
+
+
+def addresses_left_in_sector(address: int) -> int:
+    """
+    Returns the amount of valid addresses left within the user data sector.
+    This function is not valid outside that sector.
+    :param address: Current Hex address
+    :return: The amount of addresses until 0x817.
+    """
+    sector = address % 0x930
+    return 0x817 - sector
+
+
 def get_differences(file1, file2):
     differences = []
     min_length = min(len(file1), len(file2))
     i = 0
+    last_percentage = -1  # To avoid redundant prints
 
     while i < min_length:
-        if file1[i] != file2[i]:
+        # Calculate progress percentage
+        percentage = (i * 100) // min_length
+
+        # Only update if percentage changes to avoid excessive prints
+        if percentage != last_percentage:
+            last_percentage = percentage
+            sys.stdout.write(f"\rProcessing: {percentage}%")
+            sys.stdout.flush()
+
+        if file1[i] != file2[i] and is_valid_sector(i):
             hex_address = f"0x{i:X}"
             entry_type = "char"
             length = 1
+            sectors_left = addresses_left_in_sector(i)
 
-            if i + 1 < min_length and file1[i:i + 1] != file2[i:i + 1]:
+            if i + 1 < min_length and file1[i:i + 1] != file2[i:i + 1] and sectors_left >= 1:
                 length = 2
                 entry_type = "short"
 
-            if i + 3 < min_length and file1[i:i + 2] != file2[i:i + 2]:
+            if i + 3 < min_length and file1[i:i + 2] != file2[i:i + 2] and sectors_left >= 3:
                 length = 4
                 entry_type = "word"
 
@@ -45,10 +78,12 @@ def get_differences(file1, file2):
                 "address": hex_address,
                 "value": hex_value
             })
+
             i += length
         else:
             i += 1
 
+    print("\rProcessing: 100%")
     return differences
 
 
